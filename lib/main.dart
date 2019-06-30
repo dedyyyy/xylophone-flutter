713b6cbf76c8a92a9b7cd3a9f9448f070b040b5b
Dedy <dedyjoomla@gmail.com>
2019-07-01 04:46:14 +0700

    tanpa fungsi

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:audioplayers/audio_cache.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
@@ -7,8 +7,117 @@ class XylophoneApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        appBar: AppBar(
+          title: Text('Music'),
+          backgroundColor: Colors.red,
+        ),
         body: SafeArea(
-          child: Container(),
+          child: Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+
+                  Expanded(
+                    child: Container(
+                      child: FlatButton(
+                        onPressed: (){
+                          final player = AudioCache();
+                          player.play('note1.wav');
+                        },
+
+                      ),
+
+                      color: Colors.red,
+                    ),
+
+                  ),
+                  Expanded(
+                    child: Container(
+                      child: FlatButton(
+                          onPressed: (){
+                            final player = AudioCache();
+                            player.play('note2.wav');
+                          },
+
+                      ),
+
+                      color: Colors.orange,
+                    ),
+
+                  ),
+                  Expanded(
+                    child: Container(
+                      child: FlatButton(
+                          onPressed: (){
+                            final player = AudioCache();
+                            player.play('note3.wav');
+                          },
+
+                      ),
+
+                      color: Colors.yellow,
+                    ),
+
+                  ),
+
+                  Expanded(
+                    child: Container(
+                      child: FlatButton(
+                        onPressed: (){
+                          final player = AudioCache();
+                          player.play('note4.wav');
+                        },
+
+                      ),
+
+                      color: Colors.green,
+                    ),
+
+                  ),
+                  Expanded(
+                    child: Container(
+                      child: FlatButton(
+                        onPressed: (){
+                          final player = AudioCache();
+                          player.play('note5.wav');
+                        },
+
+                      ),
+
+                      color: Colors.blue,
+                    ),
+
+                  ),Expanded(
+                    child: Container(
+                      child: FlatButton(
+                        onPressed: (){
+                          final player = AudioCache();
+                          player.play('note6.wav');
+                        },
+
+                      ),
+
+                      color: Colors.indigo,
+                    ),
+
+                  ),
+                  Expanded(
+                    child: Container(
+                      child: FlatButton(
+                        onPressed: (){
+                          final player = AudioCache();
+                          player.play('note7.wav');
+                        },
+
+                      ),
+
+                      color: Colors.purple,
+                    ),
+
+                  ),
+                ]),
+          ),
         ),
       ),
     );
